7f7720df80333e2fa076f65e1be22b4dd403c87c
Tiffany Wang <thiffanyjun@hotmail.com>
2023-08-29 16:42:01 +1200

    updating server routes from controller

--- server/routes/cocktails.ts
+++ server/routes/cocktails.ts
@@ -1,18 +1,35 @@
 import { Router } from 'express'
 
-import * as db from '../db/cocktails.ts'
+import * as controller from '../controller/cocktailsController'
 
 const router = Router()
 
 router.get('/', async (req, res) => {
   try {
-    const fruits = await db.getAllFruits()
-
-    res.json({ fruits: fruits.map((fruit) => fruit.name) })
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Something went wrong' })
+    const response = await controller.getCocktails(req)
+    res.json(response)
+  } catch (err) {
+    res.status(500).send(err)
   }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const response = await controller.addCocktail(req)
+    res.json(response[0])
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const response = await controller.deleteCocktail(req)
+  res.json(response)
+})
+
+router.patch('/:id', async (req, res) => {
+  const response = await controller.updateCocktail(req)
+  res.json(response[0])
 })
 
 export default router
